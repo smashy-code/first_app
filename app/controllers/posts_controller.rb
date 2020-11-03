5f60da425a863a53463bf3d97f2c777538f8addd
smashy-code <73206032+smashy-code@users.noreply.github.com>
2020-11-03 18:54:13 +0900

    フロント側の記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,11 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.all.order(id: "DESC")
   end
-  
-
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
 end
-
 def checked
   post = Post.find(params[:id])
   if post.checked
@@ -17,8 +13,8 @@ def checked
   else
     post.update(checked: true)
   end
-
   item = Post.find(params[:id])
   render json: { post: item }
      end
+
 end
